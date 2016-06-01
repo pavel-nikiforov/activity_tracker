a767b8794dfd0ebc81d82f6b1c671666b3a72356
Pavel Nikiforov <pavel.nikiforov@gmail.com>
2016-06-01 20:38:57 +0300

    it's alive: data extraction for a single page is nealy done

--- atrack.py
+++ atrack.py
@@ -11,8 +11,15 @@ from selenium.webdriver.common.by import By
 import sys
 
 driver = None
+user_id = None
+
+my_times = []
+my_links = []
+my_descr = []
+
 username = 'username'
 password = 'password'
+
 
 
 def login():
@@ -21,7 +28,9 @@ def login():
     driver = webdriver.Firefox()
 
     # go to the google home page
-    driver.get("http://redmine.ln/projects/go-system-up-win/activity")
+    #driver.get("http://redmine.ln/projects/go-system-up-win/activity")
+    #driver.get("http://redmine.ln/projects/go-rfid/activity?from=2016-05-31")
+    driver.get("http://redmine.ln/projects/go-rfid/activity")
 
     # the page is ajaxy so the title is originally this:
     print(driver.title)
@@ -55,13 +64,34 @@ def login():
         exit()
 
 
+    global user_id
+    try:
+        user_id = driver.find_element_by_css_selector('div#loggedas a').get_attribute("href")
+        print "user id: " + user_id
+    except NoSuchElementException as e:
+        print "Login failed"
+        driver.quit()
+        exit()
+
 
 def read_tasks():
 
+    raw_times = []
+    raw_links = []
+    raw_descr = []
+    raw_users = []
+
+    global my_links
+    global my_descr
+
     try:
-        tasknames = driver.find_elements_by_css_selector('div#activity dl:first-of-type dt.issue-edit a')
+        tasknames = driver.find_elements_by_css_selector('div#activity dl:first-of-type dt a')
         for taskname in tasknames:
-            print taskname.get_attribute("href")
+            link = taskname.get_attribute("href")
+            descr = taskname.text
+            print "%s\n%s" % (link,descr)
+            raw_links.append(link)
+            raw_descr.append(descr)
     except NoSuchElementException as e:
         print("No task names found")
 
@@ -70,11 +100,147 @@ def read_tasks():
     try:
         authors = driver.find_elements_by_css_selector('div#activity dl:first-of-type dd span.author a')
         for author in authors:
-            print author.get_attribute("href")
+            userlink = author.get_attribute("href")
+            print userlink
+            raw_users.append(userlink)
     except NoSuchElementException as e:
-        print("No task names found")
+        print("No task authors found")
+
+
+    try:
+        times = driver.find_elements_by_css_selector('div#activity dl:first-of-type dt span.time')
+        #activity > dl:nth-child(2) > dt:nth-child(3) > span:nth-child(2)
+        for time in times:
+            usertime = time.text
+            print usertime
+            raw_times.append(usertime)
+    except NoSuchElementException as e:
+        print("No times found")
+
+
+
+    for i in range(len(raw_links)):
+        if raw_users[i] == user_id:
+            my_times.append(raw_times[i])
+            my_links.append(raw_links[i])
+            my_descr.append(raw_descr[i])
+
+    print "\n\nGeneral outcome"
+    if len(my_links) > 0:
+        print "Processed by my: ", len(my_links)
+        for i in range(len(my_links)):
+            print "[%i] %s" % (i,my_times[i])
+            print "[%i] %s" % (i,my_descr[i])
+            print "[%i] %s" % (i,my_links[i])
+    else:
+        print "No activity today"
+
+
+
 
     driver.quit()
+
+
+def process_tasks():
+
+    for i in range(len(my_links)):
+        if my_descr[i].find("(") == -1:
+            my_times.remove(i)
+            my_descr.remove(i)
+            my_links.remove(i)
+        else:
+            status = my_descr[i][my_descr[i].find("(") + 1:my_descr[i].find(")")]
+            my_descr[i] = status
+
+            if my_links[i].find("#") != -1:
+                trimmed_link = my_links[i][0:my_links[i].find("#")]
+                my_links[i] = trimmed_link
+
+
+    print "\n\nProcessed (step 1):"
+    for i in range(len(my_links)):
+        #print "[%i] %s" % (i,my_descr[i])
+        print "[%i] (%s) %s - %s" % (i,my_times[i],my_links[i],my_descr[i])
+
+
+    for i in range(len(my_links)):
+        for j in range(len(my_links)-1):
+            if my_times[j] < my_times[j+1]:
+                temp_time = my_times[j]
+                temp_desc = my_descr[j]
+                temp_link = my_links[j]
+
+                my_times[j] = my_times[j+1]
+                my_descr[j] = my_descr[j+1]
+                my_links[j] = my_links[j+1]
+
+                my_times[j+1] = temp_time
+                my_descr[j+1] = temp_desc
+                my_links[j+1] = temp_link
+
+
+    print "\n\nProcessed (step 2):"
+    for i in range(len(my_links)):
+        #print "[%i] %s" % (i,my_descr[i])
+        print "[%i] (%s) %s - %s" % (i,my_times[i],my_links[i],my_descr[i])
+
+
+    print "\n"
+    processed_items_count = 0
+    total_items = len(my_links)
+
+    while(processed_items_count < total_items):
+        duplicate_list = []
+        for i in range(processed_items_count+1,len(my_links)):
+            #print "[%i] (%s) %s - %s" % (i,my_times[i],my_links[i],my_descr[i])
+            if (my_links[i] == my_links[processed_items_count]):
+                #print "will remove [%i] (%s) %s - %s" % (i+1,my_times[i],my_links[i],my_descr[i])
+                duplicate_list.append(i)
+
+        #print duplicate_list
+        duplicates_count = 0
+        for i in duplicate_list:
+                del my_times[i-duplicates_count]
+                del my_descr[i-duplicates_count]
+                del my_links[i-duplicates_count]
+                duplicates_count = duplicates_count + 1
+
+
+        total_items = len(my_links)
+        processed_items_count = processed_items_count + 1
+
+
+    print "\n\nProcessed (step 3):"
+    for i in range(len(my_links)):
+        #print "[%i] %s" % (i,my_descr[i])
+        print "[%i] (%s) %s - %s" % (i,my_times[i],my_links[i],my_descr[i])
+
+
+
+def translate_statuses():
+    print "\n\nProcessed (step 4):"
+    for i in range(len(my_links)):
+        # New
+        if my_descr[i] == u'\u041d\u043e\u0432\u0430\u044f':
+            my_descr[i] = 'New'
+        # Closed
+        if my_descr[i] == u'\u0420\u0435\u0448\u0435\u043d\u0430':
+            my_descr[i] = 'Closed'
+        if my_descr[i] == u'\u0417\u0430\u043a\u0440\u044b\u0442\u0430':
+            my_descr[i] = 'Closed'
+        # Fix Again
+        if my_descr[i] == u'\u041d\u0430 \u0434\u043e\u0440\u0430\u0431\u043e\u0442\u043a\u0443':
+            my_descr[i] = 'Fix Again'
+        # In Progress
+        if my_descr[i] == u'\u0412 \u0442\u0435\u0441\u0442\u0438\u0440\u043e\u0432\u0430\u043d\u0438\u0438':
+            my_descr[i] = 'Testing In Progress'
+
+        print "[%i] (%s) %s - %s" % (i,my_times[i],my_links[i],my_descr[i])
+
+
+
+
+
 
 
 
@@ -89,3 +255,5 @@ if __name__ == '__main__':
 
     login()
     read_tasks()
+    process_tasks()
+    translate_statuses()
